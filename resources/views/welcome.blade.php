69ed2408c308620b6b77ae47fb3a9912cfd06dc0
cedricfortuinroc <78347141+cedricfortuinroc@users.noreply.github.com>
2021-12-15 16:21:03 +0100

    yeet

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,4 +3,6 @@
     <div class="text-center">
         biertje?
     </div>
+
+    <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rick-astley-1-1552336336.png" alt="ah">
 </x-guest-layout>
