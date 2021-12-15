b2a38d7011d36ad7563de4b025a841398f8e9232
cedricfortuinroc <78347141+cedricfortuinroc@users.noreply.github.com>
2021-12-15 15:50:51 +0100

    unsafe

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -36,13 +36,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed'],
+            //Rules\Password::defaults()
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => $request->password,
         ]);
 
         event(new Registered($user));
--- resources/views/livewire/post-by-slug.blade.php
+++ resources/views/livewire/post-by-slug.blade.php
@@ -40,7 +40,7 @@
         </div>
         <div class="mt-2 md:flex md:items-center md:justify-between">
             <div class="flex-1 min-w-0">
-                <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+                <h2 id="title" class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
                     {{ $post->title }}
                 </h2>
             </div>
